0516039d3aee3ca06d919996ab1fa7767a980f9e
Nuno Pereira <nunosp15@hotmail.com>
2021-07-03 19:49:23 +0100

    Conect to internet

--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Net;
 using RestSharp;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Security;
 
 namespace ReadExcel
 {
@@ -14,7 +17,7 @@ namespace ReadExcel
         static void Main(string[] args)
         { 
             // Abrir arquivo Excel
-            var xls = new XLWorkbook(@"C:\Users\3100945\Documents\data.xlsx");
+            var xls = new XLWorkbook(@"C:\Users\Nuno\Documents\data.xlsx");
 
 
             var planilha = xls.Worksheets.First(w => w.Name == "Folha1");
@@ -43,11 +46,6 @@ namespace ReadExcel
                 Console.Write("".PadLeft(15));
                 Console.WriteLine(Convert.ToInt32(planilha.Cell("B" + linha.ToString()).Value.ToString()));
 
-
-                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("https://claimopenpt.duckdns.org/auth/login/");
-
-                myReq.GetResponse();
-                
                 linha++;
 
             }
@@ -60,6 +58,63 @@ namespace ReadExcel
 
             Console.ReadKey();
         
+        }
+
+        protected void LinkWebpage(object sender, EventArgs e)
+        {
+            //Vai ligar-se ao portal de pedidos
+
+            WebRequest request = WebRequest.Create("https://claimopenpt.duckdns.org/auth/login");
+            request.Method = "GET";
+            request.Credentials = new NetworkCredential("nuno.pereira@pt.softinsa.com", "init1234");
+            WebResponse response = request.GetResponse();
+
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            //envia para o servidor a autenticação
+            Stream dataStream = response.GetResponseStream();
+         
+        }
+        protected void Page_Load( EventArgs e)
+        {
+
+            // Create a request using a URL that can receive a post. 
+            WebRequest request = WebRequest.Create("https://claimopenpt.duckdns.org/timesheet");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+
+            // Create POST data and convert it to a byte array.
+            string postData = "Teste";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
         }
     }
 }
