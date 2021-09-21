4eb4291f162d7018d769425037085c9b393f2834
Nuno Pereira <nuno.miguel.pereira@galp.com>
2021-09-21 19:22:53 +0100

    Implementation registry websiteFix conection to site and select objets Angular

--- ReadExcel/LigaSite.cs
+++ ReadExcel/LigaSite.cs
@@ -5,32 +5,82 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
+using ReadExcel;
 
 namespace ToolsQA
 {
-    class ConetaSite
+    public class ConetaSite 
     {
         IWebDriver driver;
 
-        [SetUp]
+     
         public void IniciaLigacao()
         {
 
             driver = new FirefoxDriver();
-
+            Console.WriteLine("vou ligar!!");
         }
 
-        [Test]
+       
         public void AbreLigacao()
         {
-            driver.Navigate().GoToUrl("https://claimopenpt.duckdns.org/auth/login");
+            Console.WriteLine("A ligar...");
+
+            IWebDriver liga = new FirefoxDriver();
+
+            liga.Navigate().GoToUrl("https://claimopenpt.duckdns.org/auth/login");
+
+            // Faz a ligação com as credenciais de acesso
+
+            liga.FindElement(By.Name("email")).SendKeys("nuno.pereira@pt.softinsa.com");
+            Console.WriteLine("Login entered");
+
+            liga.FindElement(By.Name("password")).SendKeys("init1234");
+            Console.WriteLine("password entered");
+
+            liga.FindElement(By.TagName("button")).Click();
+
+            liga.Navigate().GoToUrl("https://claimopenpt.duckdns.org/timesheet");
+
+            // Vai para a semana especifica
+            liga.FindElement(By.CssSelector("[ng-click='addObject()']")).Click();
+
+            // Colocar Data do registo
+            liga.FindElement(By.CssSelector("[ng-model='obj.weekday']")).SendKeys("2021-10-11");
+
+            // Colocar ID do projeto e o tipo de projeto
+            //liga.FindElement(By.XPath("//button[.=' Insert Line ']")).Click();
+            liga.FindElement(By.CssSelector("[ng-model='obj.type_id']")).SendKeys("IN PROJECT");
+
+            //Adiciona o pedido para registo de hora
+            liga.FindElement(By.CssSelector("[class='glyphicon glyphicon-search']")).Click();
+            liga.FindElement(By.CssSelector("[ng-model='searchCond']")).SendKeys("S190710_000430");
+            liga.FindElement(By.CssSelector("[ng-click='initWorkObjects(1, searchCol, searchCond)']")).Click();
+            //liga.FindElement(By.XPath("//ng-repeat='props in scopeProp.data'//'client_id':'S190710_000430','summary':'COLOCAÇÃO DE FONTES / BINÁRIOS / POWERBUILDER'}']")).Click();
+            liga.FindElement(By.CssSelector("[ng-click='handleSelectClick(props)']")).Click();
+
+            // Colocar Consultoria
+
+            liga.FindElement(By.CssSelector("[ng-options='activity.activity_id']")).SendKeys("CS");
+
+            // Colocar Horas de registo
+
+            liga.FindElement(By.CssSelector("[ng-options='activity.activity_id']")).SendKeys("CS");
+            liga.FindElement(By.CssSelector("[ng-model='value']")).SendKeys("0.5");
+            liga.FindElement(By.CssSelector("[ng-model='obj.claim_type']")).SendKeys("Normal");
+
+            // Colocar Id do catalogo OpenSGC
+
+            liga.FindElement(By.CssSelector("[ng-model='obj.observations']")).SendKeys("17016");
         }
 
-        [TearDown]
+
         public void FechaLigacao()
         {
-            driver.Close(); //fecha o browser
+            Console.WriteLine("Adeus!");
+            driver.Quit(); //fecha o browser
 
         }
         
--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -4,15 +4,16 @@ using ClosedXML.Excel;
 using System.Text;
 using System.Net;
 using System.IO;
-
+using ToolsQA;
 
 namespace ReadExcel
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // Abrir arquivo Excel
+            //var xls = new XLWorkbook(@"C:\Users\3100945\Documents\data.xlsx"); -< PC do trabalho
             var xls = new XLWorkbook(@"C:\Users\Nuno\Documents\data.xlsx");
 
 
@@ -22,7 +23,7 @@ namespace ReadExcel
             Console.WriteLine("Os dados no ficheiro Excel são:\n");
 
             Console.WriteLine("".PadRight(55, '-'));
-            Console.WriteLine("Nome".PadRight(10) + "Idade".PadLeft(15) + "Distrito".PadLeft(20));
+            Console.WriteLine("Nome".PadRight(10) + "Distrito".PadLeft(20));
             Console.WriteLine("".PadRight('-'));
 
             // primeira linha é o cabecalho
@@ -40,7 +41,7 @@ namespace ReadExcel
 
                 Console.Write(nome.PadRight(5));
                 Console.Write("".PadLeft(15));
-                Console.WriteLine(Convert.ToInt32(planilha.Cell("B" + linha.ToString()).Value.ToString()));
+                Console.WriteLine(Convert.ToString(planilha.Cell("B" + linha.ToString()).Value.ToString()));
                 Console.Write("".PadLeft(35));
                 Console.WriteLine(Convert.ToString(planilha.Cell("C" + linha.ToString()).Value.ToString()));
 
@@ -53,7 +54,12 @@ namespace ReadExcel
             xls.Dispose();
 
             Console.WriteLine("".PadRight(10, '-'));
-            Console.WriteLine("Feito!!");
+
+            ConetaSite liga_ao_site = new ConetaSite();
+
+            liga_ao_site.AbreLigacao();
+            liga_ao_site.IniciaLigacao();
+            liga_ao_site.FechaLigacao();
 
             Console.ReadKey();
         }
